5901bd22531028277f8da37c06120836011a7f9a
hadeel-4 <ha986562@gmail.com>
2021-07-28 12:50:05 +0300

    change

--- ITGDevices/Controllers/DevicesRequestController.cs
+++ ITGDevices/Controllers/DevicesRequestController.cs
@@ -5,8 +5,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ITGDevices.Models;
+using System.Net.Mail;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+using System.Net;
+using System.IO;
 
 namespace ITGDevices.Controllers
 {
@@ -66,37 +69,73 @@ namespace ITGDevices.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> RequestDevice(ItemOperation obj)
+        public  IActionResult RequestDevice(ItemOperation obj)
         {
             if ((string.Compare(HttpContext.Session.GetString("role"), "Employee", true) == 0))
             {
 
 
-                var item = obj.item;
-                if (item == null)
+                /* var item = obj.item;
+
+
+                         var listOfusersId = _context.userRoles.Where(r => r.roleID == 2).ToList();
+                         List<User> managers = new List<User>();
+                         foreach (UserRole r in listOfusersId)
+                         {
+                             var u = _context.users.Single(e => e.ID == r.userID);
+                             managers.Add(u);
+
+                         }
+                         User manager = managers[0];
+                         int uid = (int)HttpContext.Session.GetInt32("id");
+                         User caurrentUser = _context.users.Single(e => e.ID == uid);
+   */
+               // UserItem h = _context.UserItem.Single(i => i.ItemID == item.ID);
+                // User holder = _context.users.Single(i => i.ID == h.UserID);
+                // ItemOperation itemOperation = new ItemOperation();
+                //itemOperation.item = item;
+                // itemOperation.holder = holder;
+
+                MailMessage mail = new MailMessage();
+                mail.From = new System.Net.Mail.MailAddress("ha412233@gmail.com");
+                SmtpClient smtp = new SmtpClient();
+                smtp.Port = 587;
+                smtp.EnableSsl = true;
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new NetworkCredential(mail.From.Address, "hadeel 123456789");
+
+                smtp.Host = "smtp.gmail.com";
+
+                //recipient
+                mail.To.Add(new MailAddress("ha986562@gmail.com"));
+
+                mail.IsBodyHtml = true;
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+
+                StreamReader reader = new StreamReader($"{Directory.GetCurrentDirectory()}/wwwroot/files/Body.cshtml");
+                string readFile = reader.ReadToEnd();
+
+                
+                 mail.Body = readFile;
+                mail.Subject = "Devices";
+
+                try
                 {
-                    return NotFound();
+                    smtp.Send(mail);
+                    System.Diagnostics.Debug.WriteLine("done");
                 }
-                if (!item.IsActive)
+                catch (SmtpException ex)
                 {
 
+                    Console.WriteLine(ex.StackTrace);
                 }
-                else
-                {
-
-                }
-                 UserItem h = _context.UserItem.Single(i => i.ItemID == item.ID);
-                User holder = _context.users.Single(i => i.ID == h.UserID);
-                ItemOperation itemOperation = new ItemOperation();
-                itemOperation.item = item;
-                itemOperation.holder = holder;
+               
+                
+                
 
 
-
-
-                return View(itemOperation);
-
-                // return View(user);
+                return RedirectToAction("Index", "DevicesRequest");
             }
             else return RedirectToAction("Login", "users");
         }
